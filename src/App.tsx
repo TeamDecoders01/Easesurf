55f392d97c1f289cbde6a1cb713965644ce54820
Shaneel Reddy <shaneelreddy320@gmail.com>
2025-03-10 08:18:25 +0530

    refactor: update manifest permissions and reorganize imports in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,38 @@
-import './App.css';
-import type { ReactElement } from 'react';
-import Scroll from "./components/Scroll";
-import Reader from "./components/Reader";
+import "./App.css";
+import type { ReactElement } from "react";
 import ChatUI from "./components/ChatUI";
 import Summarize from "./components/Summarize";
-import Reader from './components/Reader';
-import Scroll from './components/Scroll';
-import SimpleUI from './components/SimpleUI';
-import Mag from './components/Mag';
-import ContrastMode from './components/ContrastMode';
-import AutoClickControl from './components/AutoClickControl';
-        import BlueScreenFilter from './components/BlueScreenFilter';
-import ReminderServices from './components/ReminderServices';
+import Reader from "./components/Reader";
+import Scroll from "./components/Scroll";
+import SimpleUI from "./components/SimpleUI";
+import Mag from "./components/Mag";
+import ContrastMode from "./components/ContrastMode";
+import AutoClickControl from "./components/AutoClickControl";
+import BlueScreenFilter from "./components/BlueScreenFilter";
+import ReminderServices from "./components/ReminderServices";
 
 export default function App(): ReactElement {
     return (
-        <div className="p-2 space-y-4">
-            <div>
-                <SimpleUI />
-            </div>
-            <div>
-                <Scroll />
-            </div>
-            <div>
-                <Reader />
-            </div>
-            <div>
-                <BlueScreenFilter />
-            </div>
-            <div className="mt-6 p-4 border rounded-md bg-gray-50">
-                <ReminderServices />
-            </div>
-        </div>
-                <ChatUI />
+        <>
+            <div className="p-2 space-y-4">
+                <div>
+                    <SimpleUI />
+                </div>
+                <div>
+                    <Scroll />
+                </div>
+                <div>
+                    <Reader />
+                </div>
+                <div>
+                    <BlueScreenFilter />
+                </div>
+                <div className="mt-6 p-4 border rounded-md bg-gray-50">
+                    <ReminderServices />
+                </div>
+                <div>
+                    <ChatUI />
+                </div>
             </div>
             <div>
                 <Summarize />
